8b4a33168d2a921dc48c61d028ad680028d67f5d
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2020-12-31 22:35:59 +0700

    login page

--- get-a/src/components/pages/users/Login.js
+++ get-a/src/components/pages/users/Login.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Input, Row } from 'antd';
 
 export default function Login() {
+    // const {} = useForm
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-page">
+            <Row className="input-center">
+                <span className="h1 white">GET-A</span>
+           
+                <Input className="input-login" />
+                <Input className="input-login"  />
+                </Row>
         </div>
     )
 }

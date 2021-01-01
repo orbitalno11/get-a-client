8dac75c74b41f9316631f5cc4328f5416edea34c
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-01-01 14:03:13 +0700

    validate

--- get-a/src/components/pages/users/Login.js
+++ get-a/src/components/pages/users/Login.js
@@ -1,16 +1,54 @@
 import React from 'react'
-import { Input, Row } from 'antd';
+import { Button } from 'antd';
+import { useForm } from "react-hook-form";
+import { loginSchema } from '../../../validation/validation'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
+
+
 
 export default function Login() {
-    // const {} = useForm
+
+
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(loginSchema),
+    });
+    const onSubmit = data => {
+        alert(JSON.stringify(data));
+    };
+
     return (
-        <div className="login-page">
-            <Row className="input-center">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {
+                console.log(errors)
+            }
+            <div className="login-page">
                 <span className="h1 white">GET-A</span>
-           
-                <Input className="input-login" />
-                <Input className="input-login"  />
-                </Row>
-        </div>
+                <div className="input-form">
+
+                    <p className="title ">อีเมล</p>
+                    <input className="input" type="email" name="email" ref={register} />
+                    {
+                        errors.email && <p className="error-input">{errors.email.message}</p>
+                    }
+                    <p className="title margintop10">รหัสผ่าน</p>
+                    <input className="input" name="password" type="password" ref={register} />
+                    {
+                        errors.password && <p className="error-input">{errors.password.message}</p>
+                    }
+
+                </div>
+                <Button className="buttonBlueColor margintop20" shape="round" size="large" htmlType="submit">เข้าสู่ระบบ</Button>
+                <div className="margintop10" >
+                    <Link to="/register">
+                        <Button className="buttonText" type="link" >สมัครสมาชิก</Button>
+                    </Link>
+
+                </div>
+            </div>
+
+        </form>
     )
+
+
 }
---
+++ get-a/src/validation/validation.js
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+});
+
+
+export {
+    loginSchema
+}

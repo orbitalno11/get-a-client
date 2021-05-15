36cb20da7dc7fbaf805b43293d90202693fed25c
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-15 19:26:32 +0700

    remove line

--- src/components/defaultFunction/checkEmptyObject.js
+++ src/components/defaultFunction/checkEmptyObject.js
@@ -1,5 +1,3 @@
 export default function isEmpty(data) {
     return data === null || data === undefined || (typeof(data) === 'object' && Object.keys(data).length === 0) || (typeof(data)  === 'string' && data.trim().length === 0)
 }
-
-

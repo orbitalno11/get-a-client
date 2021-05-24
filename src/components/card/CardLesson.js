dcf223f98b5c902018de7535ba4615fb5e48b1bb
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-24 20:34:35 +0700

    reduce page and merge page offline , online and lesson page
    card
    
    revise manage course page
    
    set state
    
    fixx route

--- src/components/card/CardLesson.js
+++ src/components/card/CardLesson.js
@@ -1,34 +1,33 @@
 import React from "react"
-import { Card, Image, Row, Col, Grid } from "antd";
+import { Image, Row, Col } from "antd";
 import styles from "./styles.module.scss"
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const { useBreakpoint } = Grid;
+import { color } from "../defaultValue";
 
 export default function CardLesson({ data }) {
-    const screens = useBreakpoint();
-    return (
-        <Card className={screens.xs ? (styles.cardLessonXS) : (styles.cardLesson)} >
-            <Row>
-                <Col xs={13} sm={10} md={10} lg={10} xl={10} className={styles.gridImage}>
-                    <Image
-                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                        className={styles.imageClip}
-                        preview={false}
-                    />
+    const colorFont = {
+        color : color.black
+    }
+        return (
+        <Row className={`${styles.card} ${styles.paddingCard} `} style={colorFont} align="middle">
+            <Col xs={8} sm={8} md={8} lg={8} xl={10}>
+                <Image
+                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                    className={styles.imageClip}
+                    preview={false}
+                />
+            </Col>
+            <Col xs={15} sm={14} md={14} lg={14} xl={14} style={{ paddingLeft: "1.75rem" }} align="start">
+                <Col className={`${styles.titleH5} ${styles.cutText1Line}`} span={24}>{data && data.title}</Col>
+                <Col className={styles.textNormal} span={24}>ตอนที่ {data && data.episode}</Col>
+                <Col span={24} className={`${styles.textSmall} ${styles.cutText1Line}`} style={{ marginTop: "0.5rem" }} >
+                    ความยาวตอน   {data && data.time} ชั่วโมง</Col>
+                <Col span={24} style={{ marginTop: "0.5rem" }}>
+                    <FontAwesomeIcon icon={faCoins} className={styles.rate} />
+                    <span className={styles.textIcon}>ฟรี</span>
                 </Col>
-                <Col xs={11} sm={14} md={14} lg={14} xl={14} style={{ paddingTop: "0.5rem" }}>
-                    <Col className={styles.titleH5} span={24}>{data && data.title}</Col>
-                    <Col className={styles.textNormal} span={24}>ตอนที่ {data && data.episode}</Col>
-                    <Col span={24} className={styles.textSmall} style={{ marginTop: "0.5rem" }} >
-                        ความยาวตอน   {data && data.time} ชั่วโมง</Col>
-                    <Col span={24} style={{ marginTop: "0.5rem" }}>
-                        <FontAwesomeIcon icon={faCoins} className={styles.rate} />
-                        <span className={styles.textIcon}>ฟรี</span>
-                    </Col>
-                </Col>
-            </Row>
-        </Card>
+            </Col>
+        </Row>
     )
 }

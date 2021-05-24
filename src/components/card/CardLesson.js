7980b67435b992738c875d217d7c4c6f846a5893
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-24 13:45:59 +0700

    revise

--- src/components/card/CardLesson.js
+++ src/components/card/CardLesson.js
@@ -1,34 +1,34 @@
 import React from "react"
-import { Card,Image,Row,Col,Grid} from "antd";
+import { Card, Image, Row, Col, Grid } from "antd";
 import styles from "./styles.module.scss"
-import {faCoins} from "@fortawesome/free-solid-svg-icons";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const { useBreakpoint } = Grid;
 
-export default function CardLesson({data}) {
+export default function CardLesson({ data }) {
     const screens = useBreakpoint();
     return (
-            <Card  className={screens.xs? (styles.cardLessonXS):(styles.cardLesson)} >
-                <Row>
-                    <Col xs={13} sm={10} md={10} lg={10} xl={10} className={styles.gridImage}>
-                        <Image
-                            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                            className={styles.imageClip}
-                            preview={false}
-                        />                    
+        <Card className={screens.xs ? (styles.cardLessonXS) : (styles.cardLesson)} >
+            <Row>
+                <Col xs={13} sm={10} md={10} lg={10} xl={10} className={styles.gridImage}>
+                    <Image
+                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                        className={styles.imageClip}
+                        preview={false}
+                    />
+                </Col>
+                <Col xs={11} sm={14} md={14} lg={14} xl={14} style={{ paddingTop: "0.5rem" }}>
+                    <Col className={styles.titleH5} span={24}>{data && data.title}</Col>
+                    <Col className={styles.textNormal} span={24}>ตอนที่ {data && data.episode}</Col>
+                    <Col span={24} className={styles.textSmall} style={{ marginTop: "0.5rem" }} >
+                        ความยาวตอน   {data && data.time} ชั่วโมง</Col>
+                    <Col span={24} style={{ marginTop: "0.5rem" }}>
+                        <FontAwesomeIcon icon={faCoins} className={styles.rate} />
+                        <span className={styles.textIcon}>ฟรี</span>
                     </Col>
-                    <Col xs={11} sm={14} md={14} lg={14} xl={14} style={{paddingTop:"0.5rem"}}>
-                        <Col className={styles.titleH5} span={24}>{data && data.title}</Col>
-                        <Col className={styles.textNormal} span={24}>ตอนที่ {data && data.episode}</Col>
-                        <Col span={24} className={styles.textSmall} style={{marginTop:"0.5rem"}} >
-                            ความยาวตอน   {data && data.time} ชั่วโมง</Col>
-                        <Col span={24} style={{marginTop:"0.5rem"}}>
-                            <FontAwesomeIcon icon={faCoins} className={styles.rate}/>
-                            <span className={styles.textIcon}>ฟรี</span>
-                        </Col>
-                    </Col>
-                </Row>
-            </Card>
+                </Col>
+            </Row>
+        </Card>
     )
 }
--- src/components/pages/users/managecourse/onlineCourse/clip/ManageClipDetail.js
+++ src/components/pages/users/managecourse/onlineCourse/clip/ManageClipDetail.js
@@ -1,57 +1,57 @@
-import { Col, Grid, Row, Typography,Button } from "antd";
-import React from "react";
-import CardClip from "../../../../../card/CardClip";
-import ListClip from "../../../../../card/ListClip";
-import styles from "../../styles.module.scss";
-import { courseOnline } from "../../../../../card/Constants";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import isMobile from "../../../../../isMobile/isMobile"
-const { useBreakpoint } = Grid;
-const { Link } = Typography;
+// import { Col, Grid, Row, Typography,Button } from "antd";
+// import React from "react";
+// import CardClip from "../../../../../card/CardClip";
+// import ListClip from "../../../../../card/ListClip";
+// import styles from "../../styles.module.scss";
+// import { courseOnline } from "../../../../../card/Constants";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import isMobile from "../../../../../isMobile/isMobile"
+// const { useBreakpoint } = Grid;
+// const { Link } = Typography;
 
-export default function ManageCourseDetail() {
-  const screens = useBreakpoint();
+// export default function ManageCourseDetail() {
+//   const screens = useBreakpoint();
 
-  return (
-    <div>
-      {isMobile() ? (
-        <div>
-          {courseOnline &&
-            courseOnline.map((item, index) => (
-              <div key={index}>
-                <Link href="/tutor/online/{courseId}">
-                  <ListClip data={item} />
-                </Link>
-              </div>
-            ))}
-          <div className={styles.marginRigth}>
-            <Link href="/tutor/online/create">
-              <Button
-                className="backgroundBlue buttonColor"
-                shape="circle"
-                icon={
-                  <FontAwesomeIcon icon={faPlus} style={{ color: "white" }} />
-                }
-              />
-            </Link>
-          </div>
-        </div>
-      ) : (
-        <Row className={!screens.lg ? styles.alignCenter : styles.pagecard}>
-          {courseOnline &&
-            courseOnline.map((item, index) => (
-              <Col
-                className={!screens.lg ? styles.paddindMd : styles.paddingmange}
-                key={index}
-              >
-                <Link href="/tutor/online/{courseId}">
-                  <CardClip data={item} />
-                </Link>
-              </Col>
-            ))}
-        </Row>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {isMobile() ? (
+//         <div>
+//           {courseOnline &&
+//             courseOnline.map((item, index) => (
+//               <div key={index}>
+//                 <Link href="/tutor/online/{courseId}">
+//                   <ListClip data={item} />
+//                 </Link>
+//               </div>
+//             ))}
+//           <div className={styles.marginRigth}>
+//             <Link href="/tutor/online/create">
+//               <Button
+//                 className="backgroundBlue buttonColor"
+//                 shape="circle"
+//                 icon={
+//                   <FontAwesomeIcon icon={faPlus} style={{ color: "white" }} />
+//                 }
+//               />
+//             </Link>
+//           </div>
+//         </div>
+//       ) : (
+//         <Row className={!screens.lg ? styles.alignCenter : styles.pagecard}>
+//           {courseOnline &&
+//             courseOnline.map((item, index) => (
+//               <Col
+//                 className={!screens.lg ? styles.paddindMd : styles.paddingmange}
+//                 key={index}
+//               >
+//                 <Link href="/tutor/online/{courseId}">
+//                   <CardClip data={item} />
+//                 </Link>
+//               </Col>
+//             ))}
+//         </Row>
+//       )}
+//     </div>
+//   );
+// }

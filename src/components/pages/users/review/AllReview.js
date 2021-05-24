0885fac470b5d8bde40e4fab25f3983ab2507aff
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-24 20:34:35 +0700

    parent ed42826231b1808cfb6031346a762dcd35bbc691 author Saynampung Akkararuengmontri <pung33_@hotmail.co.th> 1621863275 +0700 committer Saynampung Akkararuengmontri <pung33_@hotmail.co.th> 1622052829 +0700
    parent ed42826231b1808cfb6031346a762dcd35bbc691
    author Saynampung Akkararuengmontri <pung33_@hotmail.co.th> 1621863275 +0700
    committer Saynampung Akkararuengmontri <pung33_@hotmail.co.th> 1622052618 +0700
    
    reduce page and merge page offline , online and lesson page
    
    card
    
    revise manage course page
    
    set state
    
    created course
    
    remove online course
    
    revise card
    
    CardCourse
    
    card
    
    ~
    
    size
    
    ~
    
    add action and revise manage course page
    
    get card
    
    ~
    
    get list and get detail course
    
    ~
    
    get card
    
    get list and get detail course
    
    ~
    
    change position button
    
    ~
    
    ~
    
    get list and get detail course
    
    ~
    
    ~
    
    get list and get detail course
    
    ~
    
    change position button
    
    ~
    
    ~
    
    create clip and update clip
    
    create clip course
    
    ~
    
    ~
    
    ~
    
    create clip and update clip
    
    create clip course
    
    ~
    
    ~
    
    ~
    
    ~
    
    update , create and preview
    
    Increase timeout
    
    ~
    
    ~
    
    remove log
    
    remove lof
    
    change route
    
    fix bug
    
    edit backpage and check owner clip
    
    goback
    
    ~
    
    set HD
    
    video HD

--- src/components/pages/users/review/AllReview.js
+++ src/components/pages/users/review/AllReview.js
@@ -14,7 +14,7 @@ import isEmpty from "../../../defaultFunction/checkEmptyObject";
 export default function AllReview() {
     const dispatch = useDispatch()
     const { id, videoId } = useParams()
-    const { offlineCourse, auth, review, onlineCourse  } = useSelector(state => state)
+    const { offlineCourse, auth, review, onlineCourse } = useSelector(state => state)
     const course = videoId ? (onlineCourse.clip && onlineCourse.data) : (offlineCourse.data && offlineCourse.data)
     const owner = (course && auth) && (auth.profile === course.owner.id)
 
@@ -22,61 +22,61 @@ export default function AllReview() {
     const myReview = !isEmpty(review.reviews) ? review.reviews.filter(value => value.owner === true)[0] : []
 
     useEffect(() => {
-        if(videoId){
+        if (videoId) {
             dispatch(reviewActions.getReviewClip(videoId))
-        }else{
+        } else {
             dispatch(reviewActions.getReviewByCourse(id, 1))
         }
-       
-        return()=>{
+
+        return () => {
             dispatch(reviewActions.clearReview())
         }
     }, [])
 
     const handleOpenReviewForm = (id, action) => {
-        if(action !== "delete"){
+        if (action !== "delete") {
             dispatch(modalAction.openModal({
                 body: <ReviewForm idReview={id} />,
                 size: sizeModal.default,
             }))
-        }else{
+        } else {
             dispatch(modalAction.openModal({
-                body : <DeleteForm idReview={id} type="review"/>,
+                body: <DeleteForm idReview={id} type="review" />,
                 size: sizeModal.default
             }))
         }
-       
+
     }
 
     return (
         <Fragment>
-                <div className={style.marginTop20}>
-                    {
-                        !isEmpty(review.reviews) ? (
-                            <Row>
-                                {!isEmpty(myReview) &&
-                                    <Col span={24} >
-                                        <CardReview data={myReview} myReview={true} handleEdit={handleOpenReviewForm} />
-                                    </Col>
-                                }
-                                {!isEmpty(reviewList) && reviewList.map((item, index) => (
-                                    <Col span={24} key={index} className={style.paddingTop} >
-                                        <CardReview data={item} myReview={false} />
-                                    </Col>
-                                ))}
-                            </Row>
-                        ) : (
-                            <div align="center">
-                                <EmptyImage size="default" />
-                                <p className={style.textNormal}>บทเรียนนี้ยังไม่มีผู้แสดงความคิดเห็น&nbsp;
+            <div className={style.marginTop20}>
+                {
+                    !isEmpty(review.reviews) ? (
+                        <Row>
+                            {!isEmpty(myReview) &&
+                                <Col span={24} >
+                                    <CardReview data={myReview} myReview={true} handleEdit={handleOpenReviewForm} />
+                                </Col>
+                            }
+                            {!isEmpty(reviewList) && reviewList.map((item, index) => (
+                                <Col span={24} key={index} className={style.paddingTop} >
+                                    <CardReview data={item} myReview={false} />
+                                </Col>
+                            ))}
+                        </Row>
+                    ) : (
+                        <div align="center">
+                            <EmptyImage size="default" />
+                            <p className={style.textNormal}>บทเรียนนี้ยังไม่มีผู้แสดงความคิดเห็น&nbsp;
                                     {
-                                        owner ? " อย่าลืมบอกให้นักเรียนมาแสดงคิดเห็นกันนะ" : "สมัครเรียนเพื่อแสดงความคิดเห็นสิ"
-                                    }
-                                </p>
-                            </div>
-                        )
-                    }
-                </div>
+                                    owner ? " อย่าลืมบอกให้นักเรียนมาแสดงคิดเห็นกันนะ" : "สมัครเรียนเพื่อแสดงความคิดเห็นสิ"
+                                }
+                            </p>
+                        </div>
+                    )
+                }
+            </div>
         </Fragment>
     )
 }

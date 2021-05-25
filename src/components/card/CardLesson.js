87119b8f14c20feb2ac33cd42d8b8feaf407b608
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-25 13:27:57 +0700

    ~

--- src/components/card/CardLesson.js
+++ src/components/card/CardLesson.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Image, Row, Col } from "antd";
 import styles from "./styles.module.scss"
-import { faBookReader, faCoins } from "@fortawesome/free-solid-svg-icons";
+import { faBookReader, faCoins, faClock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Fragment } from "react";
 
@@ -11,20 +11,19 @@ export default function CardLesson({ data, isCourse }) {
         <Row className={`${styles.card} ${styles.paddingCard}`} align="middle">
             <Col xs={9} sm={8} md={8} lg={10} xl={10}>
                 <Image
-                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                    src={data ? data.coverUrl : ""}
                     className={styles.imageClip}
                     preview={false}
                 />
             </Col>
-            <Col xs={15} sm={14} md={14} lg={12} xl={14} align="start" style={{paddingLeft:"0.5rem"}}>
-                <Col className={`${styles.titleH5} ${styles.cutText1Line}`} span={24}>
-                    {data && data.title}
+            <Col xs={15} sm={14} md={14} lg={12} xl={14} align="start" className={styles.marginLeft}>
+                <Col span={24}>
+                    <span className={`${styles.titleH5} ${styles.cutText1Line}`}>{data && data.name}</span>
                 </Col>
-                <Col className={styles.paddingTop} span={24}>{!isCourse ?"ตอนที่ "+data.episode : data.name}</Col>
-                <Col span={24} className={`${styles.textSmall} ${styles.cutText1Line}`} style={{ marginTop: "0.5rem" }} >
+                <Col span={24} className={`${styles.paddingTop} ${styles.textNormal} ${styles.cutText1Line}`}  >
                     {
                         !isCourse ? (
-                           <span> ความยาวตอน   {data && data.time} ชั่วโมง</span>
+                            <span > ความยาวตอน   {data && data.time} ชั่วโมง</span>
                         ) : (
                             <Fragment>
                                 <FontAwesomeIcon icon={faBookReader} className={styles.icon} />
@@ -33,8 +32,8 @@ export default function CardLesson({ data, isCourse }) {
                         )
                     } </Col>
                 <Col className={styles.paddingTop} span={24} >
-                    <FontAwesomeIcon icon={faCoins} className={styles.rate} />
-                    <span className={styles.textIcon}>ฟรี</span>
+                    <FontAwesomeIcon icon={!isCourse ? faCoins : faClock} className={styles.icon} />
+                    <span className={styles.marginLeft}>ฟรี</span>
                 </Col>
             </Col>
         </Row>

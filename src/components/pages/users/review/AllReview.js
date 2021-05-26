b696cdab7f45256c7de6280d84006f65f4f6530c
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-26 23:15:36 +0700

    ~

--- src/components/pages/users/review/AllReview.js
+++ src/components/pages/users/review/AllReview.js
@@ -7,7 +7,7 @@ import { modalAction, reviewActions } from "../../../../redux/actions";
 import { sizeModal } from "../../../modal/SizeModal";
 import ReviewForm, { DeleteForm } from "./ReviewForm";
 import { Link, useParams } from "react-router-dom";
-import FormEnroll from "../managecourse/offlineCourse/FormEnroll";
+import FormEnroll from "../managecourse/manageCourse/FormEnroll";
 import EmptyImage from "../../../loading/EmptyImage";
 import { useEffect } from "react";
 import isEmpty from "../../../defaultFunction/checkEmptyObject";

f3cb829616d5cf4e67fee50cb716aee5075f6343
Jira-A <60649487+Jira-A@users.noreply.github.com>
2021-05-26 18:06:43 +0700

    valida

--- src/validation/admin/exchangeSchema.js
+++ src/validation/admin/exchangeSchema.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export  const exchangeSchema = yup.object().shape({
-    baht: yup.number().required(),
-    coin: yup.number().required(),
+    baht: yup.number().required("กรุณากำหนดค่าเงิน"),
+    coin: yup.number().required("กรุณากำหนดค่าเหรียญ"),
 });
--- src/validation/admin/promotionSchema.js
+++ src/validation/admin/promotionSchema.js
@@ -1,5 +1,28 @@
 import * as yup from "yup";
+import moment from "moment";
 
 export  const promotionSchema = yup.object().shape({
     name: yup.string().required(), 
+    start: yup
+    .string()
+    .required("กรุณาระบุเวลาที่เริ่มโปรโมชั่น"),
+    end: yup
+    .string()
+    .required("กรุณาระบุเวลาสิ้นสุดโปรโมชั่น")
+    .test("is-greater", "เวลาสิ้นสุดโปรโมชั่นควรที่จะมีเวลามากกว่าเวลาเริ่มโปรโมชั่น", function (value) {
+      const { start } = this.parent;
+      return moment(value, "HH:mm").isSameOrAfter(moment(start, "HH:mm"));
+    }),
+    startDate: yup
+    .string()
+    .required("กรุณาระบุวันที่เริ่มโปรโมชั่น"),
+    endDate: yup
+    .string()
+    .required("กรุณาระบุวันสิ้นสุดโปรโมชั่น")
+    .test("is-greater", "เวลาสิ้นสุดโปรโมชั่นควรที่จะมีเวลามากกว่าเวลาเริ่มโปรโมชั่น", function (value) {
+      const { start } = this.parent;
+      return moment(value,"YYYY-MM-DD").isSameOrAfter(moment(start, "YYYY-MM-DD"));
+    }),
+    baht: yup.number().required("กรุณากำหนดค่าเงิน"),
+    coin: yup.number().required("กรุณากำหนดค่าเหรียญ"),
 });

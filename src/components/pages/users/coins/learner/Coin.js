47929d2f5409f20c3d30383c509b451984a1cb1c
Jira-A <60649487+Jira-A@users.noreply.github.com>
2021-05-27 13:39:46 +0700

    +-+*

--- src/components/pages/users/coins/learner/Coin.js
+++ src/components/pages/users/coins/learner/Coin.js
@@ -1,41 +1,24 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 import { Col, Row, Grid, Divider } from "antd";
 import style from "../styles.module.scss";
 import CoinDetail from "./CoinDetail";
 import Header from "../../../../headerMobile/Header";
 import Aboutcoin from "../learner/AboutCoin";
 import isMobile from "../../../../isMobile/isMobile";
-import { useDispatch, useSelector } from "react-redux";
-import { coinAction } from "../../../../../redux/actions";
 const { useBreakpoint } = Grid;
 
 export default function Coin() {
   const screens = useBreakpoint();
 
-  const dispatch = useDispatch();
-
-  const list = useSelector((state) => state.coin.rateCoin);
-
-  const loading = useSelector((state) => state.loading.loading);
-
-  useEffect(() => {
-    dispatch(coinAction.getCoinRatesLearner());
-  }, []);
-
   return (
     <Fragment>
       {isMobile() && <Header title="ร้านค้า" />}
-      {!loading ? (
-        <Row className={style.body}>
-          {list &&
-            list
-              .map((data, index) => (
-                <Col xs={24} sm={24} md={9} lg={8} xl={8} key={index}>
-                  <CoinDetail data={data} />
-                </Col>
-              ))}
+        <Row className={style.container}>
+           <Col xs={24} sm={24} md={9} lg={8} xl={8}>
+              <CoinDetail/>
+           </Col>
           {screens.md && (
-            <Col md={3} lg={2} xl={2}>
+            <Col md={3} lg={3} xl={2}>
               <Divider
                 type="vertical"
                 style={{ height: "100%", marginLeft: "3rem" }}
@@ -46,9 +29,6 @@ export default function Coin() {
             <Aboutcoin />
           </Col>
         </Row>
-      ) : (
-        <div className={style.loader}></div>
-      )}
     </Fragment>
   );
 }
--- src/components/pages/users/coins/learner/CoinDetail.js
+++ src/components/pages/users/coins/learner/CoinDetail.js
@@ -1,25 +1,27 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { Grid, Col, Divider, Row, Button,Typography } from "antd";
+import React, { Fragment, useEffect } from "react";
+import { Grid, Col, Divider, Row, Button, Typography } from "antd";
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "../styles.module.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { coinAction } from "../../../../../redux/actions";
+import Loading from "../../../../loading/Loading";
 const { useBreakpoint } = Grid;
 const { Link } = Typography;
 
-export default function CoinDetail({data}) {
+export default function CoinDetail() {
   const screens = useBreakpoint();
-  const profile = useSelector((state) => state.profile);
-  const [profileDetail, setProfileDetail] = useState(null);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.loading.loading);
+  const list = useSelector((state) => state.coin.rateCoin);
 
   useEffect(() => {
-    if (profile.profile) {
-      setProfileDetail(profile.profile);
-    }
-  }, [profile]);
+    dispatch(coinAction.getCoinRatesLearner());
+  }, []);
 
   return (
     <Fragment>
+      {loading.loading && <Loading />}
       <div>
         {screens.md && (
           <Col lg={24} xl={24} md={24}>
@@ -28,32 +30,35 @@ export default function CoinDetail({data}) {
         )}
       </div>
       <div>
-        <div className={style.coinshop}> 
+        <div className={style.coinshop}>
           <FontAwesomeIcon icon={faCoins} className={style.Big} />
-          <span>{profileDetail && profileDetail.coin}</span>
+          <span className={style.textLarge}>1000,0000</span>
         </div>
         <div className={style.coinlist}>
-          <Row>
-            <Col xs={7} sm={8} md={6} lg={7} xl={7}>
-              <FontAwesomeIcon icon={faCoins} className={style.coins} />
-            </Col>
-            <Col xs={8} sm={9} md={9} lg={10} xl={10}>
-              {data && data.coin} เหรียญ
-            </Col>
-            <Col xs={7} sm={7} md={9} lg={6} xl={6}>
-              <Link href="/coinshop/payment">
-                <Button
-                  className="backgroundGreen buttonColor"
-                  shape="round"
-                  size="middle"
-                  style={{ width: "100px" }}
-                >
-                  THB {data && data.baht}
-                </Button>
-              </Link>
-            </Col>
-            <Divider type="horizontal" style={{ height: "100%" }} />
-          </Row>
+          {list &&
+            list.map((item, index) => (
+              <Row key={index}>
+                <Col xs={7} sm={8} md={5} lg={6} xl={6}>
+                  <FontAwesomeIcon icon={faCoins} className={style.coins} />
+                </Col>
+                <Col xs={8} sm={9} md={10} lg={10} xl={10} className={style.textNormal}>
+                  {item && item.coin} เหรียญ
+                </Col>
+                <Col xs={7} sm={7} md={9} lg={6} xl={6}>
+                  <Link href="/coinshop/payment">
+                    <Button
+                      className="backgroundGreen buttonColor"
+                      shape="round"
+                      size="middle"
+                      style={{ width: "100px" }}
+                    >
+                      <span className={style.textNormal}>THB {item && item.baht}</span>
+                    </Button>
+                  </Link>
+                </Col>
+                <Divider type="horizontal" style={{ height: "100%" }} />
+              </Row>
+            ))}
         </div>
       </div>
     </Fragment>

5fbf53f5de3a3dfc05a78491c9819bceae001985
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-05-31 11:56:36 +0700

    button

---
+++ src/components/defaultFunction/style.js
@@ -0,0 +1,10 @@
+const buttonFull = (buttonColor, size) =>{
+    return ({
+        backgroundColor : buttonColor,
+        width  : size ? size : "100%"
+    })
+}
+
+export  const styleComponent = {
+    buttonFull
+} 

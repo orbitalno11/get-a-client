2b30071a6dbe143b92958944be6b8b4e3ac853c5
Jira-A <60649487+Jira-A@users.noreply.github.com>
2021-06-01 17:12:35 +0700

    nav

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -45,8 +45,9 @@ const NavMenu = () => {
                 {/* <Menu.Item key="/course" >
                     <NavLink to="/course">
                         คอร์สเรียน
-                    </NavLink>
+                </NavLink>
                 </Menu.Item>
+                */}
                 <Menu.Item key="/search" >
                     <NavLink to="/search">
                         ค้นหา

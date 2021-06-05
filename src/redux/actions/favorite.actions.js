29ae108f9d1e41e8a2ec4159681d488f6a67cb79
Jira-A <60649487+Jira-A@users.noreply.github.com>
2021-06-05 22:51:28 +0700

    dup

--- src/redux/actions/favorite.actions.js
+++ src/redux/actions/favorite.actions.js
@@ -65,28 +65,6 @@ function checkFavoriteTutor(tutorId){
     
 }
 
-function checkFavoriteTutor(tutorId){ 
-    return  dispatch => {
-        dispatch(loadingActions.startLoading())
-         apiURL.apiGetA.get("/favorite/liked", {
-            params: {
-                tutor : tutorId
-            }
-        })
-        .then((res) => {
-            dispatch(loadingActions.stopLoading())
-            const favorite = res.data.data
-            dispatch(success(favorite))
-        }).catch(err => {
-            dispatch(loadingActions.stopLoading())
-            dispatch(failure(err.response.data))
-        })
-    }
-    function success(data) { return { type: favoriteConstants.GET_CHECK_FAVORITE_SUCCESS, payload: data } }
-    function failure(err) { return { type: favoriteConstants.GET_CHECK_FAVORITE_FAILURE, payload: err } }
-    
-}
-
 function clearListFavorite() {
     return (dispatch) => { dispatch({ type: favoriteConstants.CLEAR_LIST_FAVORITE }) }
 }

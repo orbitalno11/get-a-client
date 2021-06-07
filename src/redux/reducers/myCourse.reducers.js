db4c9cddd41f597505fd9c295ef463f39eae6252
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-06-07 15:59:29 +0700

    remove log

--- src/redux/reducers/myCourse.reducers.js
+++ src/redux/reducers/myCourse.reducers.js
@@ -7,7 +7,6 @@ const initialState = {
 }
 
 const myCourseReducer = (state = initialState, action) => {
-    console.log(action)
     switch (action.type) {
         case myCourseConstants.MY_OFFLINE_COURSE_SUCCESS:
             return {

32a647e1e7a0ba30e4527626180218e1f13cb194
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-06-12 16:19:53 +0700

    active bar

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 import { Menu, Button, Row, Col } from "antd";
 import { NavLink } from "react-router-dom";
 import {
@@ -10,7 +10,7 @@ import {
     faSearch,
     faHeart
 } from "@fortawesome/free-solid-svg-icons";
-
+import isEmpty from "./defaultFunction/checkEmptyObject"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../redux/actions/auth.actions";
@@ -18,17 +18,56 @@ import isMobile from "./isMobile/isMobile";
 import { styleComponent } from "./defaultFunction/style";
 import { color } from "./defaultValue";
 import style from "./../stylesDefault.module.scss"
+import { useEffect } from "react";
 
 const NavMenu = () => {
     const auth = useSelector(state => state.auth)
     const dispatch = useDispatch()
-    const statusTutor = auth.role === 2
-    const status = auth.isAuthenticated
-    const isLearner = auth.role === 1
-    const UserMenu = () => {
+    const [currentPath, setCurrentPath] = useState("/")
+    const pathName = window.location.pathname
+    const device = isMobile() ? "mobile" : "desktop"
+    
+    const pathFocus = {
+        "tutor": ["/", "/tutor", "/tutor/course", "/tutor/online", "/me"],
+        "learner": {
+            "mobile": ["/", "/course", "/search", "/favorite", "/me"],
+            "desktop": ["/", "/popular", "/search", "/favorite", "/me"]
+        },
+        "visitor": ["/", "/search"]
+    }
 
+    const statusUser = () => {
+        if (auth.role === 2) {
+            return "tutor"
+        } else if (auth.role === 1) {
+            return "learner"
+        } else if (!auth.isAuthenticated) {
+            return "visitor"
+        }
+    }
+
+    const checkPath = () => {
+        if (statusUser() === "tutor" || statusUser() === "visitor") {
+            return pathFocus[statusUser()].find(value => value === pathName)
+        } else if (statusUser() === "learner") {
+            return pathFocus[statusUser()][device].find(value => value === pathName)
+        }
+    }
+
+    useEffect(() => {
+        if(!isEmpty(checkPath())){
+            handleChangePath(checkPath())
+        }
+    }, [pathName])
+
+    const handleChangePath = (path) => {
+        setCurrentPath(path)
+    }
+
+
+    const UserMenu = () => {
         return (
-            <Menu className="menuBar" mode="horizontal" defaultSelectedKeys={["/"]} >
+            <Menu className="menuBar" onClick={(e) => handleChangePath(e.key)} selectedKeys={[currentPath]} mode="horizontal" defaultSelectedKeys={["/"]} >
                 <Menu.Item key="/">
                     <NavLink to="/">
                         หน้าแรก
@@ -39,41 +78,35 @@ const NavMenu = () => {
                         ติวเตอร์
                     </NavLink>
                 </Menu.Item>
-                {/* <Menu.Item key="/course" >
-                    <NavLink to="/course">
-                        คอร์สเรียน
-                </NavLink>
-                </Menu.Item>
-                */}
                 <Menu.Item key="/search" >
                     <NavLink to="/search">
                         ค้นหา
-                </NavLink>
+                    </NavLink>
                 </Menu.Item>
                 {
-                    isLearner && (
+                    statusUser() === "learner" && (
                         <Menu.Item key="/favorite" >
                             <NavLink to="/favorite">
                                 รายการที่ถูกใจ
-                        </NavLink>
+                            </NavLink>
                         </Menu.Item>
                     )
                 }
                 {
-                    status ? (
+                    statusUser() !== "visitor" ? (
                         <Menu.Item key="/me" >
                             <NavLink to="/me">
                                 โปรไฟล์
-                        </NavLink>
+                            </NavLink>
                         </Menu.Item>
                     ) : null
                 }
                 {
-                    status ? (
+                    statusUser() !== "visitor" ? (
                         <Menu.Item key="/logout" onClick={() => dispatch(userActions.logout())}>
                             <NavLink to="/logout">
                                 ออกจากระบบ
-                        </NavLink>
+                            </NavLink>
                         </Menu.Item>
                     ) : null
                 }
@@ -83,38 +116,33 @@ const NavMenu = () => {
 
     const TutorMenu = () => {
         return (
-            <Menu className="menuBar" mode="horizontal" defaultSelectedKeys={["/tutor"]} >
+            <Menu className="menuBar" mode="horizontal" onClick={(e) => handleChangePath(e.key)} selectedKeys={[currentPath]} defaultSelectedKeys={["/tutor"]} >
                 <Menu.Item key="/tutor" >
                     <NavLink to="/tutor">
                         หน้าแรก
-                </NavLink>
+                    </NavLink>
                 </Menu.Item>
                 <Menu.Item key="/tutor/course"  >
                     <NavLink to="/tutor/course">
                         จัดการคอร์สเรียน
-                </NavLink>
+                    </NavLink>
                 </Menu.Item>
                 <Menu.Item key="/tutor/online"  >
                     <NavLink to="/tutor/online">
                         จัดการคอร์สออนไลน์
-                </NavLink>
+                    </NavLink>
                 </Menu.Item>
-                {/* <Menu.Item key="/notification"  >
-                    <NavLink to="/notification">
-                        แจ้งเตือน
-                </NavLink>
-                </Menu.Item> */}
                 {
-                    status ? (
+                    statusUser() !== "visitor" ? (
                         <Menu.Item key="/me" >
                             <NavLink to="/me">
                                 โปรไฟล์
-                        </NavLink>
+                            </NavLink>
                         </Menu.Item>
                     ) : null
                 }
                 {
-                    status ? (
+                    statusUser() !== "visitor" ? (
                         <Menu.Item key="/logout" onClick={() => dispatch(userActions.logout())}>
                             <NavLink to="/logout">
                                 ออกจากระบบ
@@ -129,30 +157,30 @@ const NavMenu = () => {
 
     const UserMenuMobile = () => {
         return (
-            <Row justify="space-around">
-                <Col span={4} className="iconMenu">
+            <Row justify="space-around" >
+                <Col span={4} className="iconMenu" onClick={() => handleChangePath("/")}>
                     <NavLink to="/">
-                        <FontAwesomeIcon icon={faHome} className="icon" />
+                        <FontAwesomeIcon icon={faHome} className={currentPath === "/" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
-                <Col span={4} className="iconMenu">
+                <Col span={4} className="iconMenu" onClick={() => handleChangePath("/course")}>
                     <NavLink to="/course">
-                        <FontAwesomeIcon icon={faChalkboardTeacher} className="icon" />
+                        <FontAwesomeIcon icon={faChalkboardTeacher} className={currentPath === "/course" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
-                <Col span={4} className="iconMenu">
+                <Col span={4} className="iconMenu" onClick={() => handleChangePath("/search")}>
                     <NavLink to="/search">
-                        <FontAwesomeIcon icon={faSearch} className="icon" />
-                    </NavLink>
-                </Col> 
-                <Col span={4} className="iconMenu">
-                    <NavLink to="/favorite">
-                        <FontAwesomeIcon icon={faHeart} className="icon" />
+                        <FontAwesomeIcon icon={faSearch} className={currentPath === "/search" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
-                <Col span={4} className="iconMenu">
+                <Col span={4} className="iconMenu" onClick={() => handleChangePath("/favorite")}>
+                    <NavLink to="/favorite">
+                        <FontAwesomeIcon icon={faHeart} className={currentPath === "/favorite" ? "iconActive" : "icon"} />
+                    </NavLink>
+                </Col>
+                <Col span={4} className="iconMenu" onClick={() => handleChangePath("/me")}>
                     <NavLink to="/me">
-                        <FontAwesomeIcon icon={faUserCircle} className="icon" />
+                        <FontAwesomeIcon icon={faUserCircle} className={currentPath === "/me" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
             </Row>
@@ -164,36 +192,31 @@ const NavMenu = () => {
 
     const ToturMenuMobile = () => {
         return (
-            <Row justify="space-around">
-                <Col span={6.5} className="iconMenu">
+            <Row justify="space-around" onClick={(e) => handleChangePath(e)}>
+                <Col span={6.5} className="iconMenu" onClick={() => handleChangePath("/")}>
                     <NavLink to="/">
-                        <FontAwesomeIcon icon={faHome} className="icon" />
+                        <FontAwesomeIcon icon={faHome} className={currentPath === "/" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
-                <Col span={6.5} className="iconMenu">
+                <Col span={6.5} className="iconMenu" onClick={() => handleChangePath("/tutor")}>
                     <NavLink to="/tutor">
-                        <FontAwesomeIcon icon={faTachometerAlt} className="icon" />
+                        <FontAwesomeIcon icon={faTachometerAlt} className={currentPath === "/tutor" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
 
-                <Col span={6.5} className="iconMenu">
+                <Col span={6.5} className="iconMenu" onClick={() => handleChangePath("/tutor/course")}>
                     <NavLink to="/tutor/course">
-                        <FontAwesomeIcon icon={faGraduationCap} className="icon" />
+                        <FontAwesomeIcon icon={faGraduationCap} className={currentPath === "/tutor/course" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
-                <Col span={6.5} className="iconMenu">
+                <Col span={6.5} className="iconMenu" onClick={() => handleChangePath("/tutor/online")}>
                     <NavLink to="/tutor/online">
-                        <FontAwesomeIcon icon={faChalkboardTeacher} className="icon" />
+                        <FontAwesomeIcon icon={faChalkboardTeacher} className={currentPath === "/tutor/online" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
-                {/* <Col span={6.5} className="iconMenu">
-                    <NavLink to="/">
-                        <FontAwesomeIcon icon={faBell} className="icon" />
-                    </NavLink>
-                </Col> */}
-                <Col span={6.5} className="iconMenu">
+                <Col span={6.5} className="iconMenu" onClick={() => handleChangePath("/me")}>
                     <NavLink to="/me">
-                        <FontAwesomeIcon icon={faUserCircle} className="icon" />
+                        <FontAwesomeIcon icon={faUserCircle} className={currentPath === "/me" ? "iconActive" : "icon"} />
                     </NavLink>
                 </Col>
             </Row>
@@ -210,14 +233,14 @@ const NavMenu = () => {
                     <Row align="middle">
                         <Col >
                             {
-                                statusTutor ? <TutorMenu /> : <UserMenu />
+                                statusUser() === "tutor" ? <TutorMenu /> : <UserMenu />
                             }
                         </Col>
                         {
-                            !status ? (
-                                <Col>
+                            statusUser() === "visitor" ? (
+                                <Col className={style.textOne25}>
                                     <NavLink to="/login">
-                                        <Button id="/login" className={`${style.buttonColor} ${style.textOne}`} style={styleComponent.buttonFull(color.blue, "auto")} shape="round" size="middle">เข้าสู่ระบบ</Button>
+                                        <Button id="/login" className={`${style.buttonColor} ${style.textOne25}`} style={styleComponent.buttonFull(color.blue, "auto")} shape="round" size="middle">เข้าสู่ระบบ</Button>
                                     </NavLink>
                                     <NavLink to="/register">
                                         <Button className="buttonText" type="link" >หรือ สมัครสมาชิก</Button>
@@ -235,7 +258,7 @@ const NavMenu = () => {
         return (
             <nav className="navbarBottom" >
                 {
-                    statusTutor ? <ToturMenuMobile /> : <UserMenuMobile />
+                    statusUser() === "tutor" ? <ToturMenuMobile /> : <UserMenuMobile />
                 }
             </nav>
         )

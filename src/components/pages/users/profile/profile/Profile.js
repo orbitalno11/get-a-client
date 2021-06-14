72af701db247dd7157b48a9a26d4286c766a8527
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-06-14 14:45:25 +0700

    fix profile

--- src/components/pages/users/profile/profile/Profile.js
+++ src/components/pages/users/profile/profile/Profile.js
@@ -1,4 +1,4 @@
-import { Col, Grid, Row } from "antd"
+import { Col, Row } from "antd"
 import React, { useEffect, useState } from "react"
 import isMobile from "../../../../isMobile/isMobile"
 import style from "./../styles.module.scss"
@@ -6,15 +6,14 @@ import ProfileRight from "./ProfileRight"
 import ProfileDetail from "./ProfileDetail"
 import ProfileHeader from "./ProfileHeader"
 import { useCallback } from "react"
-import { myCourseAction, profileAction, tutorAction , coinAction} from "../../../../../redux/actions"
+import { myCourseAction, profileAction, tutorAction, coinAction } from "../../../../../redux/actions"
 import { useSelector, useDispatch } from "react-redux"
 import { Fragment } from "react"
 import Header from "../../../../headerMobile/Header"
 import ModalComponent from "../../../../modal/ModalComponent"
-const { useBreakpoint } = Grid;
+import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint"
 
 export default function Profile() {
-    const screens = useBreakpoint();
     const dispatch = useDispatch()
     const auth = useSelector(state => state.auth)
     const profile = useSelector(state => state.profile)
@@ -23,14 +22,14 @@ export default function Profile() {
     const profilePage = window.location.pathname === "/me"
     const isTutor = auth.role === 2
     const balanceCoin = useSelector((state) => state.coin.balance);
-
+    const screens = useBreakpoint();
     useEffect(() => {
         if (profile.profile) {
             setDataHeader({
                 firstname: profile.profile.firstname,
                 lastname: profile.profile.lastname,
                 profileUrl: profile.profile.profileUrl,
-                coin : balanceCoin&&balanceCoin.amount,
+                coin: balanceCoin && balanceCoin.amount,
             })
 
             setDataDetail({
@@ -41,7 +40,7 @@ export default function Profile() {
                     lineId: profile.profile.contact.lineId ? profile.profile.contact.lineId : "ยังไม่ได้กำหนด",
                     phoneNumber: profile.profile.contact.phoneNumber ? profile.profile.contact.phoneNumber : "ยังไม่ได้กำหนด",
                 },
-                coin : balanceCoin&&balanceCoin.amount,
+                coin: balanceCoin && balanceCoin.amount,
             })
         }
     }, [profile])
@@ -71,7 +70,7 @@ export default function Profile() {
                 <Row className={style.bodyPaddingTopBottom} justify={"space-between"}>
                     <Col className={!isMobile() ? style.section : null} span={24}>
                         {
-                            profilePage &&  <ProfileHeader data={dataHeader && dataHeader} isTutor={isTutor} />
+                            profilePage && <ProfileHeader data={dataHeader && dataHeader} isTutor={isTutor} />
                         }
                     </Col>
                     <Col xl={10} lg={10} md={10} sm={24} xs={24}>
@@ -81,7 +80,7 @@ export default function Profile() {
                     </Col>
                     <Col xl={13} lg={13} md={13} sm={24} xs={24}>
                         {
-                           ((profilePage && screens.md) || (!profilePage)) &&  <ProfileRight isTutor={isTutor} />
+                           ((isTutor) || (!isTutor && screens.md)) && (<ProfileRight isTutor={isTutor} />)
                         }
                     </Col>
                 </Row>
